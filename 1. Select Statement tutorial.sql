0af112c2069d624f428a49e3bf2250d927dd6715
Lezzycan <waheedolalekan23@gmail.com>
2025-02-20 18:51:57 +0000

    Add files via upload

---
+++ 1. Select Statement tutorial.sql
@@ -0,0 +1,9 @@
+Select *
+from parks_and_recreation.employee_demographics;
+
+Select first_name,
+age
+from parks_and_recreation.employee_demographics;
+
+Select distinct gender
+from parks_and_recreation.employee_demographics;
